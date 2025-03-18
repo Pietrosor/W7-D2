d503b82d4a78ecb2703e6cc2ead86b83b1d6c29e
Pietrosor <pietrosorbo@hotmail.it>
2025-03-18 15:56:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ let count = sessionStorage.getItem("counter")
   ? Number(sessionStorage.getItem("counter"))
   : 0
 
-// Mostra il valore iniziale
 document.getElementById("timer").textContent = count
 
-// Aggiorna il contatore ogni secondo
 setInterval(() => {
   count++
   document.getElementById("timer").textContent = count
-  sessionStorage.setItem("counter", count) // Salva il valore aggiornato
+  sessionStorage.setItem("counter", count)
 }, 1000)
